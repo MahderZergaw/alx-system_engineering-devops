ed7308436317beb235fe3ebf81dab304e222a893
MahderZergaw <mahderzergaw640@gmail.com>
2023-11-29 11:38:18 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,14 @@
-#!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+
+$package = 'flask'
+package { $package:
+    ensure   => '2.1.0',
+    provider => 'pip3',
+}
+
+$package_2 = 'Werkzeug'
+
+package { $package_2:
+    ensure   => '2.1.1',
+    provider => 'pip3',
 }
